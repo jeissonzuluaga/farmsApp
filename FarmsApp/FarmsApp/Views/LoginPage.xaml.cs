5657cee7c42b0dba082664dd6eb34aae5e855cdd
jeissonzuluaga <64753330+jeissonzuluaga@users.noreply.github.com>
2020-06-12 02:18:01 -0500

    Link ¿Olvidaste tu contraseña?
    Se programó la lógica para abrri una URL al pulsar tap sobre el label ¿Olvidaste tu contraseña?. Queda pendiente probar abriendo una nueva Page.

--- FarmsApp/FarmsApp/Views/LoginPage.xaml.cs
+++ FarmsApp/FarmsApp/Views/LoginPage.xaml.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
+﻿
 namespace FarmsApp.Views
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Xamarin.Essentials;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        public ICommand TapForgot => new Command<string>(async (url) => await Launcher.OpenAsync(url));
         public LoginPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
     }
 }
