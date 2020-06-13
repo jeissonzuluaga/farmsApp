00eb8077b086a0dba9498857559cd5d87ae05d60
Walter Alejandro Soler Sanabria <55300584+waltersoler@users.noreply.github.com>
2020-06-13 14:53:02 -0500

    Identacion + Pages
    se organiza las diferentes pages y se crean algunas

---
+++ FarmsApp/FarmsApp/Views/WeaningPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FarmsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeaningPage : ContentPage
+    {
+        public WeaningPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
